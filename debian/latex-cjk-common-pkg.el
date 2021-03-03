e28a95c1b04cffb3d6055ff3a6404a59a766e0f7
Hilmar Preusse <hille42@web.de>
2021-03-03 23:35:29 +0100

    dh-elpa, first steps. Package FTBFS currently.

---
+++ debian/latex-cjk-common-pkg.el
@@ -0,0 +1,3 @@
+(define-package "cjkspace" "0.1" "This file implements cjkspace." 'nil)
+(define-package "cjktilde" "0.1" "This file implements cjktilde." 'nil)
+(define-package "cjk-enc" "0.1" "This file implements cjk-enc." 'nil)
---
+++ debian/latex-cjk-thai-pkg.el
@@ -0,0 +1,1 @@
+(define-package "thai-word" "0.1" "This file implements an algorithm to find Thai word breaks using a dictionary." 'nil)
